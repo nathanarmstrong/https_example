c50d8394b76e870114acac20ecf46b3745948894
nathanarmstrong <nathan.armstrong21@gmail.com>
2018-05-14 22:13:35 -0600

    make simple request

--- https_example.js
+++ https_example.js
@@ -1,3 +1,16 @@
-var https = require('https');
+const https = require('https');
 
-console.log('I did it!')
+const options = {
+    host: 'www.example.org',
+    path: '/'
+};
+
+const callback = function() {
+    console.log('In response handler callback!');
+}
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
